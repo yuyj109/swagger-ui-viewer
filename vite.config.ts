d4dc99a3d02021d41b340c3baeaf60d94fc6586d
yonjun.yu <yonjun.yu@tkk.tokyu.co.jp>
2025-06-22 09:39:47 +0900

    Fix: Add Buffer polyfill to Vite config for browser compatibility
    - Update vite.config.ts to include Buffer polyfill using @esbuild-plugins/node-globals-polyfill and buffer package.
    - Resolve "Buffer is not defined" error when running Swagger UI React in the browser.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// 追加
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  }
+});
